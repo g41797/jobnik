d1fbce0be9c4db40d73cb2bab85471e23cb4bdd6
g41797 <g41797@gmail.com>
2024-02-01 09:03:11 +0200

    Thu Feb  1 09:02:51 AM IST 2024

--- guard.go
+++ guard.go
@@ -6,25 +6,98 @@ package jobnik
 import (
 	"context"
 	"fmt"
+	"log"
 	"strings"
 	"sync"
 )
 
 var _ Jobnik = &guard{}
 
-type guard struct {
-	lock sync.Mutex
-	name string
-	jbnk Jobnik
+type guardState int
+
+const (
+	initallowed          guardState = iota
+	processfinishallowed guardState = iota + 1
+	nothingallowed       guardState = iota + 2
+)
+
+func (gst guardState) String() string {
+	return []string{"initallowed", "processfinishallowed", "nothingallowed"}[gst]
 }
 
-func (grd *guard) InitOnce(jsc string) error { return nil }
+type guard struct {
+	lock  sync.Mutex
+	state guardState
+	name  string
+	jbnk  Jobnik
+}
+
+func (grd *guard) InitOnce(jsc string) error {
+	if grd == nil {
+		return fmt.Errorf("InitOnce nil guard")
+	}
+
+	grd.lock.Lock()
+	defer grd.lock.Unlock()
+
+	if grd.jbnk == nil {
+		return fmt.Errorf("jobnik was not created")
+	}
+
+	if grd.state != initallowed {
+		return fmt.Errorf("Init disabled for %s", grd.state.String())
+	}
+
+	if err := grd.jbnk.InitOnce(jsc); err != nil {
+		grd.state = nothingallowed
+		return err
+	}
+
+	grd.state = processfinishallowed
+	return nil
+}
 
 func (grd *guard) Process(cncl context.Context, job Job) (JobStatus, error) {
-	return JobStatus{}, fmt.Errorf("Process is not implemented yet")
+	if grd == nil {
+		return JobStatus{}, fmt.Errorf("Process nil guard")
+	}
+
+	grd.lock.Lock()
+	defer grd.lock.Unlock()
+
+	if grd.jbnk == nil {
+		return JobStatus{}, fmt.Errorf("jobnik was not created")
+	}
+
+	if grd.state != processfinishallowed {
+		return JobStatus{}, fmt.Errorf("Process disabled for %s", grd.state.String())
+	}
+
+	return grd.jbnk.Process(cncl, job)
 }
 
-func (grd *guard) FinishOnce() error { return nil }
+func (grd *guard) FinishOnce() error {
+	if grd == nil {
+		return fmt.Errorf("FinishOnce nil guard")
+	}
+
+	grd.lock.Lock()
+	defer grd.lock.Unlock()
+
+	if grd.jbnk == nil {
+		return fmt.Errorf("jobnik was not created")
+	}
+
+	if grd.state != processfinishallowed {
+		return fmt.Errorf("Finish disabled for %s", grd.state.String())
+	}
+
+	err := grd.jbnk.FinishOnce()
+
+	grd.state = nothingallowed
+
+	return err
+}
 
 func (grd *guard) tryCreate(name string) (Jobnik, error) {
 
@@ -32,7 +105,7 @@ func (grd *guard) tryCreate(name string) (Jobnik, error) {
 
 	fact, exists := factories.Load(strings.ToLower(name))
 	if !exists {
-		return nil, fmt.Errorf("factor for %s does not exist", grd.name)
+		return nil, fmt.Errorf("factory for %s does not exist", grd.name)
 	}
 
 	if jbnk, err := fact.(JobnikFactory)(); err == nil {
@@ -45,14 +118,14 @@ func (grd *guard) tryCreate(name string) (Jobnik, error) {
 
 func storeFactory(name string, fact JobnikFactory) {
 	if len(name) == 0 {
-		panic("empty jobnik name")
+		log.Panic("empty jobnik name")
 	}
 	if fact == nil {
-		panic(fmt.Errorf("nil jobnik factory for %s", name))
+		log.Panicf("nil jobnik factory for %s", name)
 	}
 
 	if _, exists := factories.LoadOrStore(strings.ToLower(name), fact); exists {
-		panic(fmt.Errorf("jobnik factory for %s already exists", name))
+		log.Panicf("jobnik factory for %s already exists", name)
 	}
 	return
 }
